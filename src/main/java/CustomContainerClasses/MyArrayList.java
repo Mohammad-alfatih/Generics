6df86a7ce957895cfb8de4160842765b75b79ddb
Alfatih Mukhtar <alfatihmukhtar@zipcoders-MacBook-Pro-9.local>
2017-02-22 18:55:24 -0500

    FAILURE

--- src/main/java/CustomContainerClasses/MyArrayList.java
+++ src/main/java/CustomContainerClasses/MyArrayList.java
@@ -5,17 +5,39 @@ package CustomContainerClasses;
  */
 public class MyArrayList<T> {
     public Object[] container = new Object[0];
-    private int maxIndex = 0;
+    public int maxIndex = 0;
 
     MyArrayList() {}
     MyArrayList(int size) { container = new Object[size]; maxIndex = size; }
 
-    public void add(Object object) {
+    @SuppressWarnings("unchecked")
+    public <T> void add(Object object) {
         Object[] temp = new Object[maxIndex+1];
         System.arraycopy(container,0,temp,0,maxIndex);
         temp[maxIndex]= object;
-        container = temp;
+        container = (T[]) temp;
         maxIndex++;
-        //@SuppressWarnings("unchecked")
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T[] get(int index) {
+        return (T[]) container[index];
+    }
+
+    public <T> void remove(int index) {
+        container[index] = null;
+        Object[] temp = new Object[maxIndex-1];
+        int tempIndex = 0;
+        for(Object o : container) {
+            if(o != null) {
+                temp[tempIndex] = o;
+                tempIndex++;
+            }
+        }
+        container = temp;
+    }
+
+    public void set(int index, T value) {
+        container[index] = value;
     }
 }
--- src/test/java/CustomContainerClasses/MyArrayListTEST.java
+++ src/test/java/CustomContainerClasses/MyArrayListTEST.java
@@ -12,16 +12,39 @@ public class MyArrayListTEST {
     MyArrayList<Integer> arrList;
 
     @Before
-    public void setUp() throws ClassCastException {
+    public void setUp() {
         arrList = new MyArrayList<Integer>();
+        arrList.add(34);
     }
 
     @Test
-    public void addTEST(){
-        arrList.add(23);
-        arrList.add(34);
+    public void addTEST() {
+        String expected = "34, 1";
+        String actual = (Integer) arrList.container[0] + ", " + arrList.maxIndex; // <-- ONLY WORKS DUE TO (Integer) CAST
+        assertEquals("I should get back the value of the object stored in the container[] at index '0', which is 34, and the subsequent value of maxIndex, which should be 1",expected,actual);
+    }
+
+    @Test
+    public void getTEST() {
         int expected = 34;
-        int actual = (Integer) arrList.container[1];
-        assertEquals("I should get back the length of the new array created by the method",expected,actual);
+        int actual = (Integer)arrList.get(0);
+        assertEquals("This should return the value in the container[] at index '0', which is 34",expected,actual);
+    }
+
+    @Test
+    public void removeTEST() {
+        arrList.add(25);
+        arrList.remove(0);
+        int expected = 1;
+        int actual = arrList.container.length;
+        assertEquals("Return the length of the container after running this method, which should be less 1 item, and equal 1",expected,actual);
+    }
+
+    @Test
+    public void setTEST() {
+        arrList.set(0,12);
+        int expected = 12;
+        int actual = arrList.get(0);
+        assertEquals("Return the array's item at index 0 after it has been changed",expected,actual);
     }
 }
